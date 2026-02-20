7a73f60d5efa19ea5a0c4300c49509e6a04458e1
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 12:39:08 +0530

    UC8 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,30 +1,67 @@
 import java.util.Scanner;
-import java.util.Deque;
-import java.util.LinkedList;
 public class PalindromeCheckerApp{
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String word = sc.nextLine();
-        Deque<Character> deque = new LinkedList<>();
+        Node head = null, tail = null;
         for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
-        }
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-            if (front != rear) {
-                isPalindrome = false;
-                break;
+            Node newNode = new Node(word.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
-        if (isPalindrome) {
+        boolean result = isPalindrome(head);
+        if (result) {
             System.out.println("The string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
         }
         System.out.println("Program exited successfully.");
         sc.close();
+    }
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        Node temp = secondHalf;
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+        return true;
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
     }
 }
