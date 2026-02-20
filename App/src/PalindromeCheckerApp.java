e36970db4f8fd77eecf147f9cb1928e516a7e6e6
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 09:14:36 +0530

    UC2 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String word = sc.nextLine();
+        boolean isPalindrome = true;
+        int length = word.length();
+
+        // Loop runs only till half of the string length
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+                isPalindrome = false;
+                break;   // Stop checking once mismatch is found
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+        System.out.println("Program exited successfully.");
+        sc.close();
     }
 }
+
