8260e6f4eb2e921e0e32f61dea08efcdb0c2f99d
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 13:04:34 +0530

    UC11 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,27 +1,35 @@
+import java.util.Stack;
 import java.util.Scanner;
 public class PalindromeCheckerApp {
+    // Private data member (Encapsulation)
+    private String word;
+    // Constructor
+    public PalindromeCheckerApp(String word) {
+        this.word = word;
+    }
+    public boolean checkPalindrome() {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+class MainApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        boolean isPalindrome = true;
-        int start = 0;
-        int end = normalized.length() - 1;
-        while (start < end) {
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-        if (isPalindrome) {
-            System.out.println("The string \"" + input + "\" is a Palindrome (ignoring spaces & case).");
+        System.out.println("Enter the string:"); String input = sc.nextLine();
+        PalindromeCheckerApp checker = new PalindromeCheckerApp(input);
+        if (checker.checkPalindrome()) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
         }
         System.out.println("Program exited successfully.");
-        sc.close();
     }
 }
