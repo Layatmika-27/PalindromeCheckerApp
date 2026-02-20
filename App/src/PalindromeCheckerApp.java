e322b2d6a7d34117e4b6380fae80ca2f69f0bd03
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 10:31:20 +0530

    UC5 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
+import java.util.Stack;
 public class PalindromeCheckerApp {
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a string: ");
-            String word = sc.nextLine();
-            String reversed = "";
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reversed += word.charAt(i);
-            }
-            if (word.equals(reversed)) {
-                System.out.println("The string \"" + word + "\" is a Palindrome.");
-            } else {
-                System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-            }
-            System.out.println("Program exited successfully.");
-            sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String word = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
+        boolean isPalindrome = true;
+        for (int i = 0; i < word.length(); i++) {
+            char poppedChar = stack.pop();
+            if (word.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+        System.out.println("Program exited successfully.");
+        sc.close();
     }
+}
