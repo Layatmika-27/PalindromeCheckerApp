1df566e4720794fd17d23f0e034519066e6bca82
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 10:14:46 +0530

    UC4 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String word = sc.nextLine();
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter a string: ");
+                String word = sc.nextLine();
+                char[] characters = word.toCharArray();
+                int start = 0;
+                int end = characters.length - 1;
+                boolean isPalindrome = true;
+                while (start < end) {
+                    if (characters[start] != characters[end]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    start++;
+                    end--;
+                }
+                if (isPalindrome) {
+                    System.out.println("The string \"" + word + "\" is a Palindrome.");
+                } else {
+                    System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+                }
+                System.out.println("Program exited successfully.");
+                sc.close();
+            }
         }
-        if (word.equals(reversed)) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-        }
-        System.out.println("Program exited successfully.");
-        sc.close();
-    }
-}
