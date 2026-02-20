f2e215586d8b746d5f8501fe608966180c64ac33
Layatmika-27 <blayatmika08@gmail.com>
2026-02-20 12:54:06 +0530

    UC10 created

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,23 +3,25 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String word = sc.nextLine();
-        boolean result = isPalindrome(word, 0, word.length() - 1);
-        if (result) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        String input = sc.nextLine();
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome (ignoring spaces & case).");
         } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
         }
         System.out.println("Program exited successfully.");
         sc.close();
-    }
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        return isPalindrome(str, start + 1, end - 1);
     }
 }
